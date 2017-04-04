90f9017e3877ff6efb3a4e90e69a99764bfc248b
Yingru Xu <yx59@duke.edu>
2017-04-04 16:09:36 -0400

    clean up the struct a bit

--- sqr.f
+++
@@ -1,8 +0,0 @@
-      FUNCTION SQR(X)                                                           
-
-      IMPLICIT REAL*8(A-H,O-Z)
-
-C       SQUARE ROOT OF THE ABSOLUTE VALUE OF X                                  
-       SQR=SQRT(ABS(X) )                                                        
-      RETURN                                                                    
-      END                                                                       
